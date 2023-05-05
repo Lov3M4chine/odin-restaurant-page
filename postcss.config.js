d81a2cdf521d78afa7eb95924e63fdd03e5c5401
Lov3M4chine <125694577+Lov3M4chine@users.noreply.github.com>
2023-05-05 11:19:05 -0600

    Implement padding and background

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   plugins: [
+    require('postcss-import'),
+    require('postcss-url')({
+      url: 'rebase'
+    }),
     require('tailwindcss')('./tailwind.config.js'),
     require('autoprefixer'),
   ],
